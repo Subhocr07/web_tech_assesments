95bc337f28a00052b88676c7ec3ad9b7f0ce597b
Subhocr07 <96890352+Subhocr07@users.noreply.github.com>
2022-10-15 15:05:29 +0530

    calculator

---
+++ calculator/calculator/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Calculator from './components/Calculator'
+
+const App = () => {
+  return (
+    <>
+      <Calculator></Calculator>
+    </>
+  )
+}
+
+export default App
